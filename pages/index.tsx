243e63a9cfa968854ac1c413cae5635c70acdc86
thanhtrairo <tienthanhcntn789@gmail.com>
2022-06-03 09:28:39 +0700

    u

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,41 +2,41 @@ import type { GetServerSideProps } from 'next'
 
 import Head from 'next/head'
 import Header from '../src/components/header/Header'
-import Carousel from '../src/components/Carousel'
-import { Watch } from '../src/components/watch/Watch'
-import { MoveToWatch } from '../src/components/watch/MoveToWatch'
-import { Video } from '../src/components/Video'
-import { WatchListComponent } from '../src/components/watch/WatchListComponent'
-import { ExportsMovie } from '../src/components/ExportsMovie/ExportsMovie'
-import { Footer } from '../src/components/Footer'
-import { MoveToExplore } from '../src/components/MoveToExplore/MoveToExplore'
-import { AccountType, Props } from '../src/type/type'
+// import Carousel from '../src/components/Carousel'
+// import { Watch } from '../src/components/watch/Watch'
+// import { MoveToWatch } from '../src/components/watch/MoveToWatch'
+// import { Video } from '../src/components/Video'
+// import { WatchListComponent } from '../src/components/watch/WatchListComponent'
+// import { ExportsMovie } from '../src/components/ExportsMovie/ExportsMovie'
+// import { Footer } from '../src/components/Footer'
+// import { MoveToExplore } from '../src/components/MoveToExplore/MoveToExplore'
+// import { AccountType, Props } from '../src/type/type'
 import MovieServices from '~/services/MovieServices'
-import { useEffect, useState } from 'react'
-import useSWR from 'swr'
-import request from '~/utils/request'
-import { fetcher } from '~/services/fetcher'
-import { useTranslation } from 'next-i18next'
+// import { useEffect, useState } from 'react'
+// import useSWR from 'swr'
+// import request from '~/utils/request'
+// import { fetcher } from '~/services/fetcher'
+// import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-const Home = ({ moviePopular, movieTrending, personPopular, movieStreaming }: Props) => {
-  const [account, setAccount] = useState<AccountType>({ success: false, session_id: '', accountId: '', username: '' })
+const Home = () => {
+  // const [account, setAccount] = useState<AccountType>({ success: false, session_id: '', accountId: '', username: '' })
 
-  useEffect(() => {
-    const account = localStorage.getItem('account') ? JSON.parse(localStorage.getItem('account') || '') : ''
-    setAccount(account)
-  }, [])
+  // useEffect(() => {
+  //   const account = localStorage.getItem('account') ? JSON.parse(localStorage.getItem('account') || '') : ''
+  //   setAccount(account)
+  // }, [])
 
-  const { data: watchList } = useSWR(
-    account.session_id ? request.fetchWatchList(account.accountId, account.session_id) : null,
-    fetcher
-  )
+  // const { data: watchList } = useSWR(
+  //   account.session_id ? request.fetchWatchList(account.accountId, account.session_id) : null,
+  //   fetcher
+  // )
 
-  const { data: ratingList } = useSWR(
-    account.session_id ? request.fetchRatingList(account.accountId, account.session_id) : null,
-    fetcher
-  )
-  const { t } = useTranslation()
+  // const { data: ratingList } = useSWR(
+  //   account.session_id ? request.fetchRatingList(account.accountId, account.session_id) : null,
+  //   fetcher
+  // )
+  // const { t } = useTranslation()
   return (
     <>
       <Head>
@@ -45,7 +45,7 @@ const Home = ({ moviePopular, movieTrending, personPopular, movieStreaming }: Pr
       </Head>
       <div className="overflow-hidden text-[80%] sm:text-[100%]">
         <Header />
-        <main className="overflow-hidden bg-gray3 px-2 text-white">
+        {/* <main className="overflow-hidden bg-gray3 px-2 text-white">
           <div className="mx-auto sm:container">
             <Carousel movieTrending={movieTrending} />
             <div className=" mb-20">
@@ -74,36 +74,27 @@ const Home = ({ moviePopular, movieTrending, personPopular, movieStreaming }: Pr
             <MoveToExplore personPopular={personPopular} />
             <Footer />
           </div>
-        </main>
+        </main> */}
       </div>
     </>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
-  const x = await serverSideTranslations(String(locale), ['common', 'header', 'movieDetail'])
-  try {
-    const result = await Promise.all([
-      MovieServices.getPopularMovies(),
-      MovieServices.getTrendingMovies(),
-      MovieServices.getComingSoonMovies(),
-      MovieServices.getPopularPerson(),
-    ])
-    return {
-      props: {
-        ...x,
-        moviePopular: result[0].data.results,
-        movieTrending: result[1].data.results,
-        movieStreaming: result[2].data.results,
-        personPopular: result[3].data.results,
-      },
-    }
-  } catch (e) {
-    return {
-      props: {
-        ...x,
-      },
-    }
+  const result = await Promise.all([
+    MovieServices.getPopularMovies(),
+    MovieServices.getTrendingMovies(),
+    MovieServices.getComingSoonMovies(),
+    MovieServices.getPopularPerson(),
+  ])
+  return {
+    props: {
+      ...(await serverSideTranslations(String(locale), ['common', 'header', 'movieDetail'])),
+      moviePopular: result[0].data.results,
+      movieTrending: result[1].data.results,
+      movieStreaming: result[2].data.results,
+      personPopular: result[3].data.results,
+    },
   }
 }
 
